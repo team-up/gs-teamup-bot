8a4d28f927c5b4c248fe48684085dc06a94df3a6
KyoungJoo <jkj8790@estsoft.com>
2018-08-01 13:49:08 +0900

    메시지에 버튼 추가

--- src/main/java/gs/teamup/bot/service/ChatService.java
+++ src/main/java/gs/teamup/bot/service/ChatService.java
@@ -1,12 +1,13 @@
 package gs.teamup.bot.service;
 
-import gs.teamup.bot.pojo.edge.ChatMessage;
+import gs.teamup.bot.pojo.edge.*;
 import gs.teamup.bot.template.teamup.EdgeTemplate;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -25,12 +26,19 @@ public class ChatService {
 
     public void doChat(ChatMessage chatMessage, Long room) {
         String content = chatMessage.getContent().trim();
+        String responseId = chatMessage.getResponseId();
+        log.info(chatMessage.getResponseId());
         if (content.equals("?")) {
             edgeTemplate.say(room, version);
-        } else if (content.equals("로또")) {
+        } else if (content.equals("로또") || "lotto".equals(responseId)) {
             List<Integer> number = new Random().ints(1, 45).distinct().limit(6).boxed().sorted()
                     .collect(Collectors.toList());
-            edgeTemplate.say(room, number.toString());
+
+            List<Button> buttons = new ArrayList<Button>();
+            buttons.add(Button.textButton("lotto", "로또 번호 뽑기", "로또 번호 뽑아줘"));
+            ExtraV2 extraV2 = new ExtraV2(buttons, null, buttons);
+
+            edgeTemplate.say(room, number.toString(), extraV2);
         }
     }
 }
